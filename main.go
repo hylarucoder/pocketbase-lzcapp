b48cbef99af14afa6b53b7e9faedc8eb5e35c8ff
hylarucoder <hylarucoder@gmail.com>
2024-11-18 17:48:17 +0800

    add ui

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"pocketbase-lzcapp/ui"
 
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
@@ -18,17 +18,12 @@ func main() {
 	}
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// add new "GET /hello" route to the app router (echo)
-		e.Router.AddRoute(echo.Route{
-			Method: http.MethodGet,
-			Path:   "/",
-			Handler: func(c echo.Context) error {
-				return c.String(200, "Hello world!")
-			},
-			Middlewares: []echo.MiddlewareFunc{
-				apis.ActivityLogger(app),
-			},
-		})
+		// serve the index.html file
+		e.Router.GET(
+			"*",
+			echo.StaticDirectoryHandler(ui.DistDirFS, false),
+			middleware.Gzip(),
+		)
 
 		return nil
 	})
